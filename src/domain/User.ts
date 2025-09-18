a1bd2467b5982ffaef20b25dc0b9e900de72cce1
darellanodev <darellano.aliaga@gmail.com>
2025-09-18 21:35:15 +0200

    feat: Add a project to the user projects (#9)

--- src/domain/User.ts
+++ src/domain/User.ts
@@ -1,8 +1,17 @@
+import { Project } from './Project'
+
 export class User {
   id: string
   name: string
+  projects: Project[]
+
   constructor(id: string, name: string) {
     this.id = id
     this.name = name
+    this.projects = []
+  }
+
+  addProject(project: Project) {
+    this.projects.push(project)
   }
 }
--- tests/domain/User.test.ts
+++ tests/domain/User.test.ts
@@ -1,10 +1,20 @@
 import { it, expect, describe } from 'vitest'
 import { User } from '../../src/domain/User'
+import { Project } from '../../src/domain/Project'
 
 describe('User', () => {
   it('Should create a new User with id and name', () => {
     const user = new User('user-id-1', 'Alice')
     expect(user.id).toBe('user-id-1')
     expect(user.name).toBe('Alice')
+  })
+  it('should add a Project to the User projects', () => {
+    const user = new User('user-id-1', 'Alice')
+    const project = new Project('project-1', 'My first project', [])
+
+    user.addProject(project)
+
+    expect(user.projects).toContain(project)
+    expect(user.projects.length).toBe(1)
   })
 })
